f91f0f15b5a9f21abac9fbd3270c1be943080122
araujooj <gabrielrudy575@gmail.com>
2020-04-21 15:10:41 -0300

    feature: created all api - Creation of properties and users, JWT, Geolocalization

---
+++ app/Controllers/Http/ImageController.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const Image = use('App/Models/Image')
+const Helpers = use('Helpers')
+const Property = use('App/Models/Property')
+
+/**
+ * Resourceful controller for interacting with images
+ */
+class ImageController {
+  /**
+   * Create/save a new image.
+   * POST images
+   */
+  async show ({ params, response }) {
+    return response.download(Helpers.tmpPath(`uploads/${params.path}`))
+  }
+
+  async store ({ request, params }) {
+    const property = await Property.findOrFail(params.id)
+
+    const images = request.file('image', {
+      types: ['image'],
+      size: '2mb'
+    })
+
+    await images.moveAll(Helpers.tmpPath('uploads'), file => ({
+      name: `${Date.now()}-${file.clientName}`
+    }))
+
+    if (!images.movedAll()) {
+      return images.errors()
+    }
+
+    await Promise.all(
+      images
+        .movedList()
+        .map(image => property.images().create({ path: image.fileName }))
+    )
+  }
+}
+
+module.exports = ImageController
